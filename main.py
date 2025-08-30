cd91975346c7d6dafe208383cf3c5c1248e962ea
cythreal <cythreal@gmail.com>
2025-08-30 19:01:51 -0400

    Begin work on function declaration

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info, schema_get_files_info
 
 def main():
     parser = argparse.ArgumentParser(description="AI Agent Helpdesk")
@@ -39,9 +40,27 @@ def verb_check(verbose=False):
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    system_prompt = system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
     )
 
     if verbose:
